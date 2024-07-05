3c7fdd3039ce62178d69887a5b681e3070994e78
Xu,Haifan <2940985x@student.gla.ac.uk>
2024-07-05 06:39:04 +0100

    Add Constants.java class

---
+++ src/main/java/org/haifan/merlin/utils/Constants.java
@@ -0,0 +1,10 @@
+package org.haifan.merlin.utils;
+
+public class Constants {
+
+    private Constants() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String ALL_IMAGE = "image/*";
+}
