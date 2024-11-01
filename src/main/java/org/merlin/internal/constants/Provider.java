170acaf45d7897f934c3ef258dcf501354d390c5
Xu,Haifan <2940985x@student.gla.ac.uk>
2024-11-02 01:11:52 +0800

    add devcontainer

--- src/main/java/org/merlin/internal/constants/Provider.java
+++ src/main/java/org/merlin/internal/constants/Provider.java
@@ -1,5 +1,8 @@
 package org.merlin.internal.constants;
 
+/**
+ * TODO: move Provider out of internal package, users need to import this provider
+ */
 public enum Provider {
     OPENAI, GOOGLE_GEMINI, OLLAMA
 }
